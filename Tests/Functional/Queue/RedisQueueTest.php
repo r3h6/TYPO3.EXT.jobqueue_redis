62fc8c5065db1db612b17640f587e1786dd25b5a
r3h6 <r3h6@outlook.com>
2016-05-17 22:34:35 +0200

    [TASK] Queue test trait

--- Tests/Functional/Queue/RedisQueueTest.php
+++ Tests/Functional/Queue/RedisQueueTest.php
@@ -25,6 +25,8 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  */
 class RedisQueueTest extends \TYPO3\CMS\Core\Tests\FunctionalTestCase
 {
+    use \R3H6\Jobqueue\Tests\Functional\Queue\QueueTestTrait;
+
     const QUEUE_NAME = 'TestQueue';
 
     protected $coreExtensionsToLoad = array('extbase');
@@ -52,101 +54,5 @@ class RedisQueueTest extends \TYPO3\CMS\Core\Tests\FunctionalTestCase
 
         $client = $this->queue->getClient();
         $client->flushdb();
-    }
-
-    /**
-     * @test
-     */
-    public function publishAndWaitWithMessageWorks()
-    {
-        $message = new Message('Yeah, tell someone it works!');
-        $this->queue->publish($message);
-
-        $result = $this->queue->waitAndTake(1);
-        $this->assertNotNull($result, 'wait should receive message');
-        $this->assertEquals($message->getPayload(), $result->getPayload(), 'message should have payload as before');
-    }
-
-    /**
-     * @test
-     */
-    public function waitForMessageTimesOut()
-    {
-        $result = $this->queue->waitAndTake(1);
-        $this->assertNull($result, 'wait should return NULL after timeout');
-    }
-
-    /**
-     * @test
-     */
-    public function peekReturnsNextMessagesIfQueueHasMessages()
-    {
-        $message = new Message('First message');
-        $this->queue->publish($message);
-        $message = new Message('Another message');
-        $this->queue->publish($message);
-
-        $results = $this->queue->peek(1);
-        $this->assertEquals(1, count($results), 'peek should return a message');
-        /** @var Message $result */
-        $result = $results[0];
-        $this->assertEquals('First message', $result->getPayload());
-        $this->assertEquals(Message::STATE_PUBLISHED, $result->getState(), 'Message state should be published');
-
-        $results = $this->queue->peek(1);
-        $this->assertEquals(1, count($results), 'peek should return a message again');
-        $result = $results[0];
-        $this->assertEquals('First message', $result->getPayload(), 'second peek should return the same message again');
-    }
-
-    /**
-     * @test
-     */
-    public function peekReturnsNullIfQueueHasNoMessage()
-    {
-        $result = $this->queue->peek();
-        $this->assertEquals(array(), $result, 'peek should not return a message');
-    }
-
-    /**
-     * @test
-     */
-    public function waitAndReserveWithFinishRemovesMessage()
-    {
-        $message = new Message('First message');
-        $this->queue->publish($message);
-
-
-        $result = $this->queue->waitAndReserve(1);
-        $this->assertNotNull($result, 'waitAndReserve should receive message');
-        $this->assertEquals($message->getPayload(), $result->getPayload(), 'message should have payload as before');
-
-        $result = $this->queue->peek();
-        $this->assertEquals(array(), $result, 'no message should be present in queue');
-
-        $finishResult = $this->queue->finish($message);
-        $this->assertTrue($finishResult, 'message should be finished');
-    }
-
-    /**
-     * @test
-     */
-    public function countReturnsZeroByDefault()
-    {
-        $this->assertSame(0, $this->queue->count());
-    }
-
-    /**
-     * @test
-     */
-    public function countReturnsNumberOfReadyJobs()
-    {
-        $message1 = new Message('First message');
-        $this->queue->publish($message1);
-
-        $message2 = new Message('Second message');
-        $this->queue->publish($message2);
-
-        $this->assertSame(2, $this->queue->count());
     }
 }
